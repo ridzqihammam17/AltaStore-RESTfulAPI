ad03dd22f7a21207126d0324e90dd9c396c47e23
Muchammad Abdurrochman <m.abdurrochman9@gmail.com>
2021-10-24 04:18:34 +0700

    complete category test, but product test still cant work

--- api/controllers/initTest.go
+++ api/controllers/initTest.go
@@ -13,22 +13,26 @@ func setup() {
 	db := util.MysqlDatabaseConnection(config)
 
 	// cleaning data before testing
-	db.Migrator().DropTable(&models.Product{})
-	db.AutoMigrate(&models.Product{})
 	db.Migrator().DropTable(&models.Customer{})
 	db.AutoMigrate(&models.Customer{})
+	db.Migrator().DropTable(&models.Category{})
+	db.AutoMigrate(&models.Category{})
+	db.Migrator().DropTable(&models.Product{})
+	db.AutoMigrate(&models.Product{})
 	// preparate dummy data
 	var newCustomer models.Customer
 	newCustomer.Name = "Ilham"
 	newCustomer.Email = "ilham@gmail.com"
 	newCustomer.Password = "pass123"
 
+	var newCategory models.Category
+	newCategory.Name = "Category A"
+
 	var newProduct models.Product
 	newProduct.Name = "Product A"
 	newProduct.Price = 10000
 	newProduct.Stock = 100
 	newProduct.CategoryID = 1
-
 	// user dummy data with model
 	customerModel := models.NewCustomerModel(db)
 	_, err := customerModel.Register(newCustomer)
@@ -40,5 +44,9 @@ func setup() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	categoryModel := models.NewCategoryModel(db)
+	_, err = categoryModel.Add(newCategory)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
